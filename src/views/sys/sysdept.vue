bf514d104c274386fcd234ab82c11359756a176d
2768026982 <2768026982@qq.com>
2024-09-22 10:56:09 +0800

    feat: #部门管理 页面初始化

--- src/views/sys/sysdept.vue
+++ src/views/sys/sysdept.vue
@@ -2,20 +2,23 @@
   <div class="mod-menu">
     <el-form :inline="true" :model="dataForm">
       <el-form-item>
-        <!-- v-if="isAuth('sys:sysdept:save')" -->
-        <el-button type="primary" @click="addOrUpdateHandle()">{{ $t("modules.sys.sysdept.form-item-btn") }}</el-button>
+        <el-button v-if="store.isAuth('sys:sysdept:save')" type="primary" @click="addOrUpdateHandle()">{{ $t("modules.sys.sysdept.form-item-btn") }}</el-button>
       </el-form-item>
     </el-form>
     <el-table :data="dataList" row-key="deptId" border default-expand-all style="width: 100%">
-      <el-table-column prop="name" header-align="center" min-width="40%" :label="$t('modules.sys.sysdept.table-column-1')"> </el-table-column>
-      <el-table-column prop="parentName" header-align="center" align="center" min-width="10%" :label="$t('modules.sys.sysdept.table-column-2')"> </el-table-column>
-      <el-table-column prop="orderNum" header-align="center" align="center" min-width="10%" :label="$t('modules.sys.sysdept.table-column-3')"> </el-table-column>
-      <el-table-column fixed="right" header-align="center" align="center" width="180" :label="$t('modules.sys.sysdept.table-column-4')">
-        <template #default="scope">
-          <!-- v-if="isAuth('sys:sysdept:update')" -->
-          <el-button link size="small" @click="addOrUpdateHandle(scope.row.deptId)">{{ $t("modules.sys.sysdept.table-column-4-update") }}</el-button>
-          <!-- v-if="isAuth('sys:sysdept:delete')" -->
-          <el-button link size="small" @click="deleteHandle(scope.row.deptId, scope.row.name)">{{ $t("modules.sys.sysdept.table-column-4-delete") }}</el-button>
+      <el-table-column prop="name" header-align="center" min-width="40%"
+        :label="$t('modules.sys.sysdept.table-column-1')"> </el-table-column>
+      <el-table-column prop="parentName" header-align="center" align="center" min-width="10%"
+        :label="$t('modules.sys.sysdept.table-column-2')"> </el-table-column>
+      <el-table-column prop="orderNum" header-align="center" align="center" min-width="10%"
+        :label="$t('modules.sys.sysdept.table-column-3')"> </el-table-column>
+      <el-table-column fixed="right" header-align="center" align="center" width="180"
+        :label="$t('modules.sys.sysdept.table-column-4')">
+        <template v-slot="scope">
+          <el-button v-if="store.isAuth('sys:sysdept:update')" type="text" size="small" @click="addOrUpdateHandle(scope.row.deptId)">{{
+            $t("modules.sys.sysdept.table-column-4-update") }}</el-button>
+          <el-button v-if="store.isAuth('sys:sysdept:delete')" type="text" size="small" @click="deleteHandle(scope.row.deptId, scope.row.name)">{{
+            $t("modules.sys.sysdept.table-column-4-delete") }}</el-button>
         </template>
       </el-table-column>
     </el-table>
@@ -25,15 +28,17 @@
 </template>
 
 <script lang="ts" setup>
+import { useAppStore } from "@/store";
 import baseService from "@/service/baseService";
 import { treeDataTranslate } from "@/utils/utils";
-import { ref, reactive, nextTick, onMounted } from "vue";
+import { ref, nextTick, onMounted } from "vue";
 import { IObject } from "@/types/interface";
 // import AddOrUpdate from "./sysdept-add-or-update.vue";
 
 import { useI18n } from "vue-i18n";
 import { ElMessage, ElMessageBox } from "element-plus";
 const { t } = useI18n();
+let store = useAppStore();
 
 const dataForm = ref({});
 const dataList = ref<IObject[]>([]);
@@ -61,13 +66,16 @@ const addOrUpdateHandle = (id?: string) => {
 
 // 删除
 const deleteHandle = (id: string, name: string) => {
-  let message = t("modules.sys.menu.delete-confirm", { menuName: `${menuName}` }) ? t("modules.sys.menu.delete-confirm", { menuName: `${menuName}` }) : `確定對[選單名稱為${menuName}]進行[刪除]操作?`;
-  let title = t("modules.sys.menu.delete-title") ? t("modules.sys.menu.delete-title") : "提示";
-  let confirm = t("modules.sys.menu.confirmButtonText") ? t("modules.sys.menu.confirmButtonText") : "確定";
-  let cancel = t("modules.sys.menu.cancelButtonText") ? t("modules.sys.menu.cancelButtonText") : "取消";
-  let success = t("modules.sys.menu.delete-success");
+  let message = t('modules.sys.sysdept.delete-confirm', { name: `${name}` }) ? t('modules.sys.sysdept.delete-confirm', { name: `${name}` }) : `確定對[部門名稱為${name}]進行[刪除]操作?`;
+  let title = t('modules.sys.sysdept.delete-title') ? t('modules.sys.sysdept.delete-title') : "提示";
+  let confirm = t('modules.sys.sysdept.confirmButtonText') ?
+    t('modules.sys.sysdept.confirmButtonText') : "確定";
+  let cancel = t('modules.sys.sysdept.cancelButtonText') ?
+    t('modules.sys.sysdept.cancelButtonText') : "取消";
+  let success = t('modules.sys.sysdept.delete-success') ?
+    t('modules.sys.sysdept.delete-success') : "操作成功";
 
-  ElMessageBox.confirm(message, titile, {
+  ElMessageBox.confirm(message, title, {
     confirmButtonText: confirm,
     cancelButtonText: cancel,
     type: "warning"
@@ -85,7 +93,7 @@ const deleteHandle = (id: string, name: string) => {
           });
         }
       });
-    });
+    }).catch(() => {});
 };
 
 onMounted(() => {
